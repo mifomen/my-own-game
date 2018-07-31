1fa30d51919dd1f63b7c034eb774339c7b96ad20
Ilya Koshelev <mifomen@yandex.ru>
2018-07-31 14:17:52 +0300

    change js

--- src/main.js
+++ src/main.js
@@ -11,17 +11,17 @@ var points = document.querySelectorAll('.points');
 
 var BtnStartGame = document.getElementById("StartGame");
 
-// function addScript(src) {
-//   var elem = document.createElement("script");
-//   elem.src = src;
-//   document.head.appendChild(elem);
+function addScript(src) {
+  var elem = document.createElement("script");
+  elem.src = src;
+  document.head.appendChild(elem);
 
-// }
+}
 
-// document.addEventListener('DOMContentLoaded',function(evt){
-//   addScript('questions.js');
+document.addEventListener('DOMContentLoaded',function(evt){
+  addScript('questions.js');
 
-// })
+})
 
 
 BtnStartGame.addEventListener('click',function(evt){
@@ -190,152 +190,3 @@ FiveTheme[4].addEventListener('click',function(evt) {
 })
 console.log('all fine')
 
-
-var Themes = ['html','python','Интернет','SCRATCH','css']
-var AllQuestion = [
-{
-  theme:'html', 
-  question: 'расшифруйте аббревиатуру http', 
-  points: 100 
-},
-{
-  theme:'html', 
-  question: 'Что на самом деле происходит, когда пользователь вводит в браузере адрес сайта и нажимаете Enter?', 
-  points: 200 
-},
-{
-  theme:'html', 
-  question: 'Где "живут" сайты?', 
-  points: 300 
-},
-{
-  theme:'html', 
-  question: 'Какой тег надо использовать для создания заголовков?', 
-  points: 400 
-},
-{
-  theme:'html', 
-  question: 'Данная служба подсказывает, какой именно сайт хочет посетить пользователь', 
-  points: 500 
-},
-{
-  theme:'html', 
-  question: 'Сколько людей нужно, чтобы создать сайт?', 
-  points: 100 
-},
-{
-  theme:'html', 
-  question: 'Люди пишут слова, а браузеру надо написать?', 
-  points: 200,
-  answer: 'тэги'
-},
-{
-  theme:'html', 
-  question: 'Какой тег надо использовать чтобы создать абзац', 
-  points: 300 
-},
-{
-  theme:'html', 
-  question: 'Для задания оформления, тегам прописывают..', 
-  points: 400,
-  answer: 'классы \ class'
-},
-{
-  theme:'html', 
-  question: 'Назовите 3-х китов, на которых держится интернет?', 
-  points: 500,
-  answer: 'html + css + js' 
-},
-{
-  theme:'загадки', 
-  question: 'Шли два отца и два сына, нашли три апельсина. Поделили – всем по одному. Как так получилось?', 
-  points: 100,
-  answer: 'шли дед, отец и сын'
-},
-{
-  theme:'загадки', 
-  question: 'Что в России на первом месте, а во Франции на втором?', 
-  points: 200,
-  anwser:' Буква Р'
-},
-{
-  theme:'загадки', 
-  question: 'Завязать можно, а развязать нельзя', 
-  points: 300,
-  answer:'разговор'
-},
-{
-  theme:'загадки', 
-  question: 'По какому пути ещё никто никогда не ходил и не ездил?', 
-  points: 400,
-  answer: 'по млечному'
-},
-{
-  theme:'загадки', 
-  question: 'Чем кончаются день и ночь?', 
-  points: 500,
-  answer: 'мягким знаком'
-},
-{
-  theme:'css', 
-  question: 'Как расшифровывается css? ', 
-  points: 100 
-},
-{
-  theme:'css', 
-  question: 'Для чего используют css?', 
-  points: 200 
-},
-{
-  theme:'css', 
-  question: 'Почему таблицу называют именно каскадной?', 
-  points: 300 
-},
-{
-  theme:'css', 
-  question: 'Итак, язык CSS состоит из ............. и свойств.', 
-  points: 400 
-},
-{
-  theme:'css', 
-  question: 'Чем в css, отличается запись .leard от #leard', 
-  points: 500 
-},
-{
-  theme:'css', 
-  question: 'css существуется для?', 
-  points: 100 
-},
-{
-  theme:'css', 
-  question: 'Чтобы придать объекту оформление нужно описать его...', 
-  points: 200 
-},
-{
-  theme:'css', 
-  question: 'При помощи background-image можно задать?', 
-  points: 300 
-},
-{
-  theme:'css', 
-  question: 'Что появится у элемента в результате свойства border: 2px solid yellow', 
-  points: 400 
-},
-{
-  theme:'css', 
-  question: 'При помощи background-repeat можно задать?', 
-  points: 500 
-}
-]
-
-// var dds = {
-//   dada: 500,
-//   mama: 1250,
-//   getFunction: function() {
-//     console.log('mama')
-//   },
-//   getPrice: function() {
-//     console.log('papa')
-//   }
-
-// }
